d9593a94094171a667d89cc2661dffcad3a7b241
Dwiamalina Qurratuain Najla <dwiamalinajla@gmail.com>
2026-09-08 22:03:37 +0700

    Call useStoragePath on Application instance after create()

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$storagePath = (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL'))
-    ? '/tmp/storage'
-    : dirname(__DIR__) . '/storage';
-
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath($storagePath)
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -25,3 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
